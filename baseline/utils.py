889d1238b369bf5c8a270d5d61a74a43066be9da
Shammur Absar <shammurchowdhury@gmail.com>
2021-10-19 14:13:36 +0300

    Update utils.py

--- baseline/utils.py
+++ baseline/utils.py
@@ -21,6 +21,8 @@
 
 import pandas as pd
 from sklearn.metrics import f1_score, accuracy_score, precision_score, recall_score
+from sklearn.metrics import mean_squared_error
+import statistics
 
 
 ## Function to create train test from the folds given
